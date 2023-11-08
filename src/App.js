859247808bb33c5b6a598814e54be728c3922f95
Absolutiom <minh.sthk@gmail.com>
2023-11-08 23:30:32 +0100

    CHORE: create sub tree

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import axios from 'axios';
 import Layout from './components/Layout';
 import{ Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
+import Reviews from './components/reviews/Reviews';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import NotFound from './components/notFound/NotFound';
 
 
 function App() {
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
   
     const getMovies = async () =>{
 
@@ -35,10 +41,13 @@ function App() {
 
   return (
     <div className="App">
+     <Header/>
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home/>}></Route>
-        </Route>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
+          </Route>
       </Routes>
     </div>
   );
