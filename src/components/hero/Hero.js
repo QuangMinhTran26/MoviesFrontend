a889d2f91b200a57e85ff601db9e88e30eb53d1d
Absolutiom <minh.sthk@gmail.com>
2023-11-08 23:42:19 +0100

    FIX: delete button

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -35,9 +35,7 @@ const Hero = ({movies}) => {
                                     <div className="movie-buttons-container">
                                         <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
                                             <div className="play-button-icon-container">
-                                                <FontAwesomeIcon className="play-button-icon"
-                                                    icon = {faCirclePlay}
-                                                />
+                                            
                                             </div>
                                         </Link>
 
