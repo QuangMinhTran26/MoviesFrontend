84b553ca5fe433252859a29fd753a1d88f838209
Absolutiom <minh.sthk@gmail.com>
2023-11-09 00:12:02 +0100

    FEAT: New Hero/Home site

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -35,7 +35,9 @@ const Hero = ({movies}) => {
                                     <div className="movie-buttons-container">
                                         <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
                                             <div className="play-button-icon-container">
-                                            
+                                                <FontAwesomeIcon className="play-button-icon"
+                                                    icon = {faCirclePlay}
+                                                />
                                             </div>
                                         </Link>
 
