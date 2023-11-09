ede7d3fb43639c5351f0b94bba282a0356094fc4
Absolutiom <minh.sthk@gmail.com>
2023-11-09 19:25:35 +0100

    FEAT: Add play button and trailer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import axios from 'axios';
 import Layout from './components/Layout';
 import{ Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
-
+import Header from './components/header/Header'
+import Trailer from './components/trailer/Trailer'
 
 function App() {
   const [movies, setMovies] = useState();
@@ -35,9 +36,11 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies = {movies} />} ></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>        
         </Route>
       </Routes>
     </div>
