b52fc7ad93490213486ea14a42a4a53debf744e5
Absolutiom <minh.sthk@gmail.com>
2023-11-12 19:04:49 +0100

    CHORE: try to get ngrok frontend to work

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
-export default axios.create({
-    baseURL:'https://42aa-2a02-908-1576-6e40-00-c8b3.ngrok-free.app',
-    headers:{"ngrok-skip-browser-warning": "true"}
-}); 
-
 /*export default axios.create({
+    baseURL:'https://fc67-2a02-908-1576-6e40-00-c8b3.ngrok-free.app',
+    headers:{"ngrok-skip-browser-warning": "true"}
+}); */
+
+export default axios.create({
     baseURL: 'http://localhost:8080', // Replace with your backend server URL
     headers: {
       'Content-type': 'application/json',
     
     },
   });
-*/
